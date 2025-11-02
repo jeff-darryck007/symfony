202003c2b9e8d2d1bde2c13ae71e5d3d2f7fab91
angel takam <angeltakam76@gmail.com>
2025-11-02 14:23:04 +0100

    implementation des vues

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('pages/home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/gallery', name: 'app_gallery')]
+    public function gallery(): Response
+    {
+        return $this->render('pages/gallery.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('pages/about.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/team', name: 'app_team')]
+    public function team(): Response
+    {
+        return $this->render('pages/team.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
